4096ecfb50ecd70eda21d59a60112235d22bfda4
Derek1256 <dslong508@gmail.com>
2024-03-26 18:38:55 -0500

    base Questline Update
    Things are NOT Complete, things do not have dependencys yet this is just to add to github, Also: Adds a custom message for completing the main cc quest

---
+++ kubejs/server_scripts/mods/ftbquests/cc.js
@@ -0,0 +1,3 @@
+FTBQuestsEvents.completed('4ED78105FF7C804E', factory => {
+    factory.player.tell("you've completed the CC tutorial now go learn some lua and make some programs or use the given program to find some premade cool ones Program Catalog by PeakStatuePC")
+})
