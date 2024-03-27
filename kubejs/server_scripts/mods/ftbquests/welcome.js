1ee2f582328d988af0cf7705f10f8c080c951905
Derek1256 <dslong508@gmail.com>
2024-03-27 11:46:21 -0500

    Create welcome.js
    Adds a message to completing the welcome quest

---
+++ kubejs/server_scripts/mods/ftbquests/welcome.js
@@ -0,0 +1,3 @@
+FTBQuestsEvents.completed('4028EFD528F6A15B', factory => {
+    factory.player.tell("Welcome to The Factory! Hope you enjoy the pack and have fun Report any bugs you find to github!")
+})
