92697975752b2bb851ba715343dcc20a444a65a3
Derek1256 <dslong508@gmail.com>
2024-06-05 12:52:41 -0500

    ITS COMING I SWEAR

--- kubejs/startup_scripts/Factory Item.js
+++ kubejs/startup_scripts/Factory Item.js
@@ -3,5 +3,4 @@ StartupEvents.registry('item', factory => {
 // This texture may need to be redone at some point
     .texture('factory:item/medal')
     .displayName('§bFactory Medal')
-    .tooltip("§aNow go make some fun endgame items!")
 })
--- kubejs/startup_scripts/Factory Main.js
+++ kubejs/startup_scripts/Factory Main.js
@@ -1,5 +1,5 @@
 // Changes the kubejs name to Factory
-Platform.setModName('kubejs', 'Factory')
+Platform.setModName('kubejs', 'Factory Life')
 
 StartupEvents.modifyCreativeTab('kubejs:tab', factory => {
     factory.remove('factory:medal')
