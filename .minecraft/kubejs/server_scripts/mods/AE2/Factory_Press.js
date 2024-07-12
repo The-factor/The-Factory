387791e7849460f2b0b31812d79133e9e55bef88
The-Factory <dslong603@gmail.com>
2024-07-12 14:14:03 -0500

    update the automation press recipe.

---
+++ .minecraft/kubejs/server_scripts/mods/AE2/Factory_Press.js
@@ -0,0 +1,3 @@
+ServerEvents.recipes(factory => {
+    factory.shaped(Item.of('factory:automation_press', 1), ['ALA', 'ESC', 'ASA'], {S: 'ae2:singularity', A: 'extendedae:entro_ingot', L: 'ae2:logic_processor_press', S: 'ae2:silicon_press', E: 'ae2:engineering_processor_press', C: 'ae2:calculation_processor_press'});
+})
